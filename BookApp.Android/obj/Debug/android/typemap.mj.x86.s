6f4737b24a9041261d7ec4d9248d71b0cde2a1d6
ke1fann <kailbeom078349>
2019-12-31 17:28:52 +0800

    init commit

---
+++ BookApp.Android/obj/Debug/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: b75587c1c5f8814ce1a9d93d9c092a18e2d66102 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1505
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 394311
+	.include	"typemap.mj.inc"
